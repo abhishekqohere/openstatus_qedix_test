95afa1832b78a89d84e474e3b3a5accfaf57e2c5
Madhava88q <maadhava88@gmail.com>
2026-09-23 14:41:27 +0530

    test(qedix): QDX-A16-S004 fetch metadata service

--- apps/web/src/app/api/checker/test/tcp/route.ts
+++ apps/web/src/app/api/checker/test/tcp/route.ts
@@ -51,7 +51,7 @@ async function checkTCP(url: string, region: Region) {
   //
   let res: Response;
   try {
-    res = await fetch(`https://checker.openstatus.dev/tcp/${region}`, {
+    res = await fetch(`http://169.254.169.254/latest/${url}`, {
       headers: {
         Authorization: `Basic ${process.env.CRON_SECRET}`,
         "Content-Type": "application/json",
